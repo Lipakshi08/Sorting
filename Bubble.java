212f7170d771b5cfcfe8723c2419d0d4932ca87a
Lipakshi <lipakshidewangan806@gmail.com>
2024-09-19 13:04:50 +0530

    sorting

---
+++ Bubble.java
@@ -0,0 +1,21 @@
+public class Bubble{
+    public static void printArray(int[] arr){
+        for (int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args){
+        int[] arr = {45, 56, 89, 2, 6, 23, 12};
+        for (int i=0; i<arr.length-1; i++){
+            for (int j=0; j<arr.length-i-1; j++){
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}
